2b66b1b3bdcef01540c9a021ffab5eb4fe76aae6
Nate <96822493+King-odysseus@users.noreply.github.com>
2026-07-28 20:10:00 +0100

    Create accounts app with Household and UserProfile models

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # MoneyMatters apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
